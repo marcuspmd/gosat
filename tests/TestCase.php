ce72a2ffb90f74bf4e4687873e165bcbde1649fa
Marcus Paulo M Dias <marcusmazzon@gmail.com>
2025-08-22 15:19:30 -0300

    refactor: Melhorar a legibilidade do método setUp no TestCase

--- tests/TestCase.php
+++ tests/TestCase.php
@@ -12,28 +12,33 @@ abstract class TestCase extends BaseTestCase
     protected function setUp(): void
     {
         parent::setUp();
-        
+
         // Mock Vite for tests to avoid manifest not found errors
         Vite::macro('useBuildDirectory', function () {
             return $this;
         });
-        
+
         // Override Vite to return empty strings during tests
         $this->app->bind('Illuminate\Foundation\Vite', function () {
-            return new class {
-                public function __invoke($_entrypoints = [], $_buildDirectory = null) {
+            return new class
+            {
+                public function __invoke($_entrypoints = [], $_buildDirectory = null)
+                {
                     return '';
                 }
-                
-                public function __toString() {
+
+                public function __toString()
+                {
                     return '';
                 }
-                
-                public function useBuildDirectory($_buildDirectory) {
+
+                public function useBuildDirectory($_buildDirectory)
+                {
                     return $this;
                 }
-                
-                public function withEntryPoints($_entryPoints) {
+
+                public function withEntryPoints($_entryPoints)
+                {
                     return $this;
                 }
             };
